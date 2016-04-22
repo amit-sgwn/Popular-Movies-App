7f9fda10dae2395a83762cb0ed55b735a3e38911
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-22 12:39:43 +0530

    Removed unuseful autogenerated stuff from Forecast.java and its xml file

--- app/src/main/java/com/example/sangwan/popular_movie_app/Forecast.java
+++ app/src/main/java/com/example/sangwan/popular_movie_app/Forecast.java
@@ -1,30 +1,22 @@
 package com.example.sangwan.popular_movie_app;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class Forecast extends AppCompatActivity {
+
+    private ImageView imageView ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forecast);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        Picasso.with(getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
 }
