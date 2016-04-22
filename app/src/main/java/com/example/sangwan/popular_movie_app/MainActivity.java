ba492d1c3aae7205ae279d8ae89df1e8ca49fa51
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-22 20:10:26 +0530

    Chacked loading of image in mainactivity with pissaco

--- app/src/main/java/com/example/sangwan/popular_movie_app/MainActivity.java
+++ app/src/main/java/com/example/sangwan/popular_movie_app/MainActivity.java
@@ -4,16 +4,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        if (savedInstanceState == null) {
-        //   getSupportFragmentManager().beginTransaction().add(R.id.containar, new Forecast()).commit();
-        }
+    /*    if (savedInstanceState == null) {
+           getSupportFragmentManager().beginTransaction().add(R.id.containar, new ForecastFragment()).commit();
+        }*/
+        imageView = (ImageView)findViewById(R.id.imageView1);
+        Picasso.with(getApplicationContext()).load("http://i.imgur.com/DvpvklR.png").into(imageView);
     }
 
     @Override
