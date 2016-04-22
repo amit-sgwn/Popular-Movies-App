4e89e0895c0fdce13d7f3ee8be7ff5ef473ab049
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-22 12:05:39 +0530

    Important commit .I am trying to use the Picasso library to load the images .So modified the gradle.build(module) to add dependencies .Removed the forcast.java class and recreated this class using fragment which created Forecast.java and and ForecastFragment.java with 3 xml files

--- app/src/main/java/com/example/sangwan/popular_movie_app/ForecastFragment.java
+++ app/src/main/java/com/example/sangwan/popular_movie_app/ForecastFragment.java
@@ -3,7 +3,6 @@ package com.example.sangwan.popular_movie_app;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,14 +13,10 @@ public class ForecastFragment extends Fragment {
 
     public ForecastFragment() {
     }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_forecst_fregment, container, false);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        return inflater.inflate(R.layout.fragment_forecast, container, false);
     }
 }
--- app/src/main/java/com/example/sangwan/popular_movie_app/MainActivity.java
+++ app/src/main/java/com/example/sangwan/popular_movie_app/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction().add(R.id.containar, new ForecastFragment()).commit();
+           getSupportFragmentManager().beginTransaction().add(R.id.containar, new Forecast()).commit();
         }
     }
 
@@ -25,9 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
