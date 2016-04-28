0b57908e602de5e727130d83d3152d229f73ec62
amit-sgwn <y13uc022@lnmiit.ac.in>
2016-04-28 17:35:55 +0530

    code the doInBackground method to parse image

--- app/src/main/java/com/example/sangwan/popular_movie_app/ForecastFragment.java
+++ app/src/main/java/com/example/sangwan/popular_movie_app/ForecastFragment.java
@@ -1,9 +1,11 @@
 package com.example.sangwan.popular_movie_app;
 
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -11,16 +13,35 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class ForecastFragment extends Fragment {
 
+    ImageView imageView;
+    View view;
+    GridView gridView;
+    String moviesJsonStr = null;
+    String orderBy;
+    ArrayList<String> movieIdArray = new ArrayList<String>();
+
+
     public ForecastFragment() {
     }
-    ImageView imageView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -42,7 +63,7 @@ public class ForecastFragment extends Fragment {
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        inflater.inflate(R.menu.menu_forecst_fregment,menu);
+        inflater.inflate(R.menu.menu_forecst_fregment, menu);
     }
 
     @Override
@@ -50,31 +71,86 @@ public class ForecastFragment extends Fragment {
 
         // Handle sorting
         int id = item.getItemId();
-        if(id==R.id.action_refresh){
-
+        switch (id) {
+            case R.id.action_sort_p:
+                break;
+            case R.id.action_sort_r:
+                break;
         }
-        else if(id==R.id.action_sort_p){
 
-        }
-        else if(id==R.id.action_sort_r){
-
-        }
-        else{
-
-        }
         return super.onOptionsItemSelected(item);
     }
 
-    public class FatchImage extends AsyncTask<Void,Void,Void>{
+    public class FatchImage extends AsyncTask<Void, Void, String> {
 
         @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
+        protected void onPostExecute(String str) {
+            super.onPostExecute(str);
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected String doInBackground(Void... params) {
+
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+            final String FATCH_BASE_URL =
+                    "http://api.themoviedb.org/3/discover/movie?";
+            final String SORY_BY = "sort_by";
+            final String APPID_PARAM = "api_key";
+            final String OPEN_MOVIE_API_KEY = "";
+
+            Uri builtUri = Uri.parse(FATCH_BASE_URL).buildUpon()
+                    .appendQueryParameter(SORY_BY, orderBy)
+                    .appendQueryParameter(APPID_PARAM, OPEN_MOVIE_API_KEY)
+                    .build();
+
+            Log.e("url", builtUri.toString());
+
+            try {
+                URL url = new URL(builtUri.toString());
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                } else {
+                    reader = new BufferedReader(new InputStreamReader(inputStream));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        buffer.append(line + "\n");
+                    }
+                    if (buffer.length() == 0) {
+                        return null;
+                    }
+                    return moviesJsonStr = buffer.toString();
+                }
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
             return null;
         }
+
     }
 }
